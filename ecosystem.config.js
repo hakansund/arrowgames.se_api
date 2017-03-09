997262dd3fe0f6ee74481a93b1c61e8b2eb4c50f
Håkan Sund <hakan.sund@gmail.com>
2017-03-09 08:48:02 +0100

    Modified path in pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,7 +16,7 @@ module.exports = {
             'host' : 'arrowgames.se',
             'ref'  : 'origin/master',
             'repo' : 'git@github.com:hakansund/arrowgames.se_api.git',
-            'path' : '~/arrowgames.se_api',
+            'path' : `/home/${process.env.USER}/arrowgames.se_api`,
             'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js'
         }
     }
